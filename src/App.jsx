4d3d41cd8a4041f349f5f1f175b33f5929c5cc7f
martinmontesjavier <120597788+martinmontesjavier@users.noreply.github.com>
2024-02-19 20:33:35 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { Animal } from "./Animal"
-import { Titulo } from "./componentes/Titulo"
-import { bd } from "./bd"
+
+import './App.css'
 
 function App() {
 
-
-
   return (
-    <div className="p-5 flex flex-col items-center">
-      <Titulo></Titulo>
-        <div className="flex gap-3 mt-5">
-          {
-            bd.map((animal, index)=>{
-              return(
-                <Animal key = {index} nombre={animal.nombre} imagen={animal.imagen}/>
-              )
-            })
-
-          }
-      </div>
+    <div className='p-5 flex justify-align-content-center justify-content-center'>
+        <div>
+          <h1 className='text-2xl text-center '>Tailwind</h1>
+          <img className='w-[100px]' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/320px-Tailwind_CSS_Logo.svg.png" alt="" />
+        </div>
+        <div>
+          <h1 className='text-2xl text-center '>React</h1>
+          <img className='w-[150px]' src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg" alt="" />
+        </div>
+        <div>
+          <h1 className='text-2xl text-center '>Vite</h1>
+          <img className='w-[100px]' src="https://seeklogo.com/images/V/vite-logo-BFD4283991-seeklogo.com.png" alt="" />
+        </div>      
     </div>
+
   )
 }
 
